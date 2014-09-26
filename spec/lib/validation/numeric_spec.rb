bcfede664f619eb521b9cc2aceb59aa75743c3af
Londerson <londerson@gmail.com>
2014-09-26 20:26:57 -0300

    test for validate if input is numeric

--- spec/lib/validation/numeric_spec.rb
+++ spec/lib/validation/numeric_spec.rb
@@ -3,6 +3,8 @@ require "validation/numeric"
 
 describe Validation::Numeric do
   it "should return true for integer object" do
-    
+    extend Validation::Numeric
+
+    validate(5).should eq(true)
   end
 end
