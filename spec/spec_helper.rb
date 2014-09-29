c6cf1f12b89d7edcbbaf3c808c1f5122213cc965
Londerson <londerson@gmail.com>
2014-09-29 07:56:01 -0300

    config rspec for dont show deprecation warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
 RSpec.configure do |config|
   $LOAD_PATH << "../lib"
+  
+  #remove deprecation warnings
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
 
   config.color = true
 end
