ea6e8868aeae273a93b8b9d323e3698dfbd15a7a
Londerson <londerson@gmail.com>
2014-12-17 22:23:19 -0200

    refac

--- lib/validation.rb
+++ lib/validation.rb
@@ -3,26 +3,32 @@ module Validation
   require "rules/is_boolean"
 
   class << self
+
+    def set_values(input)
+      { rule: input.keys.first.to_s, input: input.values.first }
+    end
+
     def validate(input)
-      key = input.keys.first.to_s
-      value = input.values.first
-      load_module(key)
-      is_valid?(value)
+      option = set_values(input)
+      load_module(option[:rule])
+      is_valid?(option[:input])
     end
 
     def load_module(name)
       module_name = "Is#{name.capitalize}"
       extend self::Rules::const_get(module_name)
     end
+
+    def validate!(input)
+      option = set_values(input)
+      load_module(option[:rule])
+      result = is_valid?(option[:input])
+
+      if result == false
+        raise TypeError, "The input #{option[:input]} does not match the rule #{option[:rule]}"
+      else
+        result
+      end
+    end
   end
-
-  # def validate!(input, option)
-  #   options = set_values(input, option)
-  #   load_module(options[:val_module])
-  #   result = valid?(options[:input], options[:rule])
-
-  #   unless result
-  #     raise TypeError, "The input #{input} does not match the rule #{options[:option]} with option #{options[:rule]}"
-  #   end
-  # end
 end
--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -5,17 +5,33 @@ describe Validation do
   subject { Validation }
   before { subject.load_module("integer") }
 
+  it "set some values for methods" do
+    hash = { :rule=>"integer", :input=>5 }
+    expect(subject.set_values(integer: 5)).to eq(hash)
+  end
+
   it "load module" do
     expect(subject.methods.include?(:is_valid?)).to eq(true)
   end
 
   context "validate is integer" do
+    context "validate with raise error" do
+      it "should return true" do
+        expect(subject.validate!(integer: 5)).to eq(true)
+      end
+
+      it "should return false" do
+        expect{ subject.validate!(integer: "five")}.to raise_error("The input five does not match the rule integer")
+      end
+    end
+
     it "should return true" do
       expect(subject.validate(integer: 5)).to eq(true)
     end
+
     it "should return false" do
       expect(subject.validate(integer: "five")).to eq(false)
-    end    
+    end
   end
 
   context "validate is boolean" do
