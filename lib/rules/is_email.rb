9ed375e67fbbc6b75367e309acb473043d2e9dcd
Patrício dos Santos <ps@patriciodossantos.net>
2015-01-05 16:06:25 +0100

    Issue #4 Add rule for email

---
+++ lib/rules/is_email.rb
@@ -0,0 +1,12 @@
+module Validation
+	module Rules
+		module IsEmail
+			private
+				def is_valid?(input)
+					input.downcase!
+					
+					!/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.match(input).nil?
+				end
+		end
+	end
+end
