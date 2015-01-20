e9ff750cbcd5cbec0fd44a76da45ccdfaaa5c50f
Londerson <londerson@gmail.com>
2015-01-19 22:53:55 -0200

    refactoring require files on spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,18 @@
 require "codeclimate-test-reporter"
-require "resize-validation"
+require "resize/validation"
+
+require "resize/rules/is_integer"
+require "resize/rules/is_boolean"
+require "resize/rules/is_string"
+require "resize/rules/is_positive"
+require "resize/rules/is_negative"
+require "resize/rules/is_email"
+require "resize/rules/is_date"
+require "resize/rules/is_json"
+require "resize/rules/is_multiple"
+require "resize/rules/is_empty"
+require "resize/rules/is_downcase"
+require "resize/rules/is_uppercase"
 
 CodeClimate::TestReporter.start
 
