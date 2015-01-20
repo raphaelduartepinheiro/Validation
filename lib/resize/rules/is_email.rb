a40a8538be8c5a23d8e973492dd099e5f213137c
Londerson <londerson@gmail.com>
2015-01-19 22:50:44 -0200

    refactoring to accept any type of data

--- lib/resize/rules/is_email.rb
+++ lib/resize/rules/is_email.rb
@@ -4,7 +4,7 @@ module Resize
       module IsEmail
         private
           def is_valid?(input)
-            !/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.match(input).nil?
+            !/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.match(input.to_s).nil?
           end
       end
     end
