bb9dfc0c60c70c17e591f0dc58ff9bc33692427b
Londerson <londerson@gmail.com>
2015-01-22 15:32:53 -0200

    beta implementation

--- lib/resize/validation.rb
+++ lib/resize/validation.rb
@@ -1,3 +1,5 @@
+require 'delegate'
+
 module Resize
   module Validation
 
@@ -22,6 +24,37 @@ module Resize
         else
           true
         end
+      end
+
+      def validates
+        base_class = self
+        proxy = Class.new(DelegateClass(base_class)) do
+
+          @results = []
+
+          def self.validate(input)
+            option = set_values(input)
+            load_module(option[:rule])
+
+            if option[:input].kind_of?(Array)
+              @results << is_valid?(option[:input][0], option[:input][1])
+            else
+              @results << is_valid?(option[:input])
+            end
+          end
+
+        private
+          def self.set_values(input)
+            { rule: input.keys.first.to_s, input: input.values.first }
+          end
+
+          def self.load_module(name)
+            module_name = "Is#{name.capitalize}"
+            extend Rules::const_get(module_name)
+          end
+        end
+
+        yield proxy
       end
 
       private
